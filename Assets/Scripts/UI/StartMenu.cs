5284fa7aed7f014f19118615ca203cef1c4df0fa
LuisFernando93 <39248747+LuisFernando93@users.noreply.github.com>
2025-01-25 18:37:56 -0300

    Options menu added to Main Menu
    In the options, the player may adjust the game's volume

--- Assets/Scripts/UI/StartMenu.cs
+++ Assets/Scripts/UI/StartMenu.cs
@@ -1,18 +1,41 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour
 {
+    [SerializeField] private Slider _masterVolumeSlider, _musicVolumeSlider, _SFXVolumeSlider;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _masterVolumeSlider.value = SoundManager.Instance.GetVolumeFromMixer("master");
+        _musicVolumeSlider.value = SoundManager.Instance.GetVolumeFromMixer("music");
+        _SFXVolumeSlider.value = SoundManager.Instance.GetVolumeFromMixer("SFX");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void AdjustVolume(string type)
+    {
+        switch (type)
+        {
+            case "master":
+                SoundManager.Instance.ChangeMasterVolume(_masterVolumeSlider.value);
+                break;
+            case "music":
+                SoundManager.Instance.ChangeMusicVolume(_musicVolumeSlider.value);
+                break;
+            case "SFX":
+                SoundManager.Instance.ChangeSFXVolume(_SFXVolumeSlider.value);
+                break;
+            default:
+                //String incompativel
+                break;
+        }
     }
 
     public void PlayButton()
